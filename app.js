76fa36babfd0d58f92ef3d761b831f6b9f0e5a89
Kade Stroude <86539510+KadeStroude@users.noreply.github.com>
2022-01-19 03:07:19 +0000

    Create app.js

--- app.js
+++ app.js
@@ -8,67 +8,54 @@ const cache = require('./cache');
 // Format tweet text
 function formatAndSendTweet(event) {
     // Handle both individual items + bundle sales
-    const assetName = _.get(event, ['asset', 'name'], _.get(event, ['asset_bundle', 'name']));
-    const openseaLink = _.get(event, ['asset', 'permalink'], _.get(event, ['asset_bundle', 'permalink']));
+    const assetName = _.get(event, ['buy', 'properties', 'name']);
+    const tokenID = _.get(event, ['buy', 'data', 'token_id']);
 
-    const totalPrice = _.get(event, 'total_price');
+    const totalPrice = _.get(event, 'sell', 'data', 'quantity');
 
-    const tokenDecimals = _.get(event, ['payment_token', 'decimals']);
-    const tokenUsdPrice = _.get(event, ['payment_token', 'usd_price']);
-    const tokenEthPrice = _.get(event, ['payment_token', 'eth_price']);
+    const tokenDecimals = _.get(event, ['sell', 'data', 'decimals']);
+    const tokenEthPrice = _.get(event, ['sell', 'data', 'quantity']);
 
     const formattedUnits = ethers.utils.formatUnits(totalPrice, tokenDecimals);
     const formattedEthPrice = formattedUnits * tokenEthPrice;
-    const formattedUsdPrice = formattedUnits * tokenUsdPrice;
 
-    const tweetText = `${assetName} bought for ${formattedEthPrice}${ethers.constants.EtherSymbol} ($${Number(formattedUsdPrice).toFixed(2)}) #NFT ${openseaLink}`;
+    const tweetText = `${assetName} bought for ${formattedEthPrice}${ethers.constants.EtherSymbol} TokenID: ${tokenID}. See all: https://immutascan.io/address/0xac98d8d1bb27a94e79fbf49198210240688bb1ed`;
 
     console.log(tweetText);
 
-    // OPTIONAL PREFERENCE - don't tweet out sales below X ETH (default is 1 ETH - change to what you prefer)
-    // if (Number(formattedEthPrice) < 1) {
-    //     console.log(`${assetName} sold below tweet price (${formattedEthPrice} ETH).`);
-    //     return;
-    // }
+    const imageUrl = _.get(event, ['buy', 'properties', 'image_url']);
+    
+    return tweet.tweetWithImage(tweetText, imageUrl);
 
-    // OPTIONAL PREFERENCE - if you want the tweet to include an attached image instead of just text
-    // const imageUrl = _.get(event, ['asset', 'image_url']);
-    // return tweet.tweetWithImage(tweetText, imageUrl);
-
-    return tweet.tweet(tweetText);
 }
 
 // Poll OpenSea every 60 seconds & retrieve all sales for a given collection in either the time since the last sale OR in the last minute
 setInterval(() => {
-    const lastSaleTime = cache.get('lastSaleTime', null) || moment().startOf('minute').subtract(59, "seconds").unix();
+    const lastSaleTime = cache.get('lastSaleTime', null) || moment().startOf('minute').subtract(59, "seconds").toISOString();
 
-    console.log(`Last sale (in seconds since Unix epoch): ${cache.get('lastSaleTime', null)}`);
+    console.log(`Last sale: ${cache.get('lastSaleTime', null)}`);
 
-    axios.get('https://api.opensea.io/api/v1/events', {
+    axios.get('https://api.x.immutable.com/v1/orders', {
         headers: {
-            'X-API-KEY': process.env.X_API_KEY
+            Accept: '*/*'
         },
         params: {
-            collection_slug: process.env.OPENSEA_COLLECTION_SLUG,
-            event_type: 'successful',
-            occurred_after: lastSaleTime,
-            only_opensea: 'false'
+            buy_token_address: process.env.TOKEN_CONTRACT_ADDRESS,
+            status: 'filled',
+            min_timestamp: lastSaleTime,
         }
     }).then((response) => {
-        const events = _.get(response, ['data', 'asset_events']);
+        const events = _.get(response, ['result']);
 
         const sortedEvents = _.sortBy(events, function(event) {
-            const created = _.get(event, 'created_date');
+            const created = _.get(event, 'timestamp');
 
             return new Date(created);
         })
-
-        console.log(`${events.length} sales since the last one...`);
-
         _.each(sortedEvents, (event) => {
-            const created = _.get(event, 'created_date');
+            const created = _.get(event, 'timestamp');
 
-            cache.set('lastSaleTime', moment(created).unix());
+            cache.set('lastSaleTime', moment(created).toISOString());
 
             return formatAndSendTweet(event);
         });
